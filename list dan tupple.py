477e702d452aee0a3d6343790a0eec635ec36b3d
masriadiNotDev <adiganzxd@gmail.com>
2025-11-26 20:27:57 +0800

    Create list dan tupple.py

---
+++ list dan tupple.py
@@ -0,0 +1,19 @@
+# masriadi
+
+#tuple
+Warna = ("merah",
+        "kuning",
+        "hijau")
+print("list tuple  : ", Warna)
+print("lem 1:", Warna[0]) #mengakses elemen ke-1 , tupple tak bisa di ubah
+
+#List
+hewan = ["beruk",
+         "monyet",
+         "orangutan"] # list bisa diubah 
+print("Sebelum diubah:", hewan)
+
+hewan[1] = "monyet"     # mengubah isi
+hewan.append("kucing")  # menambah data 
+print("setelah diubah:", hewan)
+#perbedaan tuple dan list adalah tuple tidak bisa di ubah sedangkan list bisa di ubah
